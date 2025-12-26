a47d06e635974ca81795a40c04feec1e34541fe8
ashis <161737873+axhis8@users.noreply.github.com>
2025-12-26 08:34:07 +0100

    Show Label current size

--- script.js
+++ script.js
@@ -4,18 +4,22 @@ const sizeRange = document.querySelector(".size-range");
 const sizeLabel = document.querySelector(".size-label");
 
 sizeRange.addEventListener('change', () => {
-
-    for (let x = 0; x < 16; x++) {
+    for (let x = 0; x < sizeRange.value; x++) {
         const row = document.createElement("div");
-        for (let y = 1; y <= 16; y++) {
+        row.classList.add("row");
+        for (let y = 1; y <= sizeRange.value; y++) {
             const column = document.createElement("div");
+            column.classList.add("column");
             column.style.border = "1px solid black"
             row.appendChild(column);
         }
+
         drawingContainer.appendChild(row); 
     }
 })
 
-
+sizeRange.addEventListener('input', () => {
+    sizeLabel.textContent = `${sizeRange.value} x ${sizeRange.value}`;
+})
 
 
