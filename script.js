829d4beda9b6f5cb775ed45656896ee76254ac52
ashis <161737873+axhis8@users.noreply.github.com>
2025-12-26 07:56:34 +0100

    Add Grids

--- script.js
+++ script.js
@@ -3,15 +3,19 @@ const drawingContainer = document.querySelector(".draw-container");
 const sizeRange = document.querySelector(".size-range");
 const sizeLabel = document.querySelector(".size-label");
 
-
-
 sizeRange.addEventListener('change', () => {
-    let drawingSize = sizeRange.value;
 
-    for (let i = 0; i <= drawingSize; i++) {
-        const grid = document.createElement("div");
-        grid.style = `width: ${drawingSize}px; height: ${drawingSize}px;`
-        drawingContainer.appendChild(grid);
+    for (let x = 0; x < 16; x++) {
+        const row = document.createElement("div");
+        for (let y = 1; y <= 16; y++) {
+            const column = document.createElement("div");
+            column.style.border = "1px solid black"
+            row.appendChild(column);
+        }
+        drawingContainer.appendChild(row); 
     }
 })
+
+
+
 
